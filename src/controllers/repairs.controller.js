597f24493a8262ce4beb1d0c21a5889c4e88bd3b
Vivian La Farina <vivianlafarina@gmail.com>
2023-06-07 13:20:56 -0500

    :art:

---
+++ src/controllers/repairs.controller.js
@@ -0,0 +1,58 @@
+
+
+
+exports.findAllUsers = (req,  res) => {
+     try {
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       status: 'fail',
+       message: 'Something went very wrong! 📢',
+
+    });
+}
+},
+exports.createUser = (req,  res) => {
+     try {
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       status: 'fail',
+       message: 'Something went very wrong! 📢',
+
+    });
+}
+},
+exports.indUser = (req,  res) => {
+     try {
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       status: 'fail',
+       message: 'Something went very wrong! 📢',
+
+    });
+}
+}
+exports.update = (req,  res) => {
+     try {
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       status: 'fail',
+       message: 'Something went very wrong! 📢',
+
+    });
+}
+},
+exports.delete = (req,  res) => {
+     try {
+} catch (error) {
+    console.log(error);
+    return res.status(500).json({
+       status: 'fail',
+       message: 'Something went very wrong! 📢',
+
+    });
+}
+}
--- src/routes/repairs.routes.js
+++ src/routes/repairs.routes.js
@@ -1,7 +1,20 @@
 const express = require('express');
-const router = express.Router();
+const userController = require('./../controllers/user.controller')
 
+
+const router = express.Router();
 // TODO: Definir End points
+
+router
+.route('')
+.get(userController.findAllUsers)
+.post(userController.createUser);
+
+router 
+ .route('/:id')
+ .get( userController.findUser)
+ .patch(userController.update)
+ .delete(userController.delete);
 
 
 module.exports = router;
