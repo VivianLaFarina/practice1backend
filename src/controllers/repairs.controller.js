76a808b73c6d89aeff495ddfa42d80e4f25d3078
Vivian La Farina <vivianlafarina@gmail.com>
2023-06-12 21:25:00 -0500

    json Token

--- src/controllers/repairs.controller.js
+++ src/controllers/repairs.controller.js
@@ -1,4 +1,6 @@
-(exports.findAllRepairs = (req, res) => {
+const Repair = require('../models/repair.model');
+
+(exports.findAllRepair = (req, res) => {
   try {
   } catch (error) {
     console.log(error);
@@ -8,8 +10,16 @@
     });
   }
 }),
-  (exports.createRepair = (req, res) => {
+  (exports.createRepair = async (req, res) => {
     try {
+      const { date, userId } = req.body;
+
+      const repair = await Repair.create(date, userId);
+
+      return res.status(200).json({
+        status: 'success',
+        repair,
+      });
     } catch (error) {
       console.log(error);
       return res.status(500).json({
